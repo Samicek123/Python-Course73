9e2ceca3c4b9b8a949e5a05d7cf3e4de84f78ba0
Sam <101989477+Samicek123@users.noreply.github.com>
2022-11-14 14:40:20 +0100

    Add files via upload

---
+++ pets.py
@@ -0,0 +1,6 @@
+def mmm_pets (pet_name,pet_type='dog'):
+    """Display informations about pet"""
+    print (f'\nThe pet is a {pet_type.title()}.')
+    print (f"\n{pet_type.title()}'s name is: {pet_name}.")
+
+mmm_pets (pet_name='hej_dobry_pomalu')
